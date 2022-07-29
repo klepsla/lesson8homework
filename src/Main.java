c78f2a443b97b60b0909c28c8c628fc7913a31a4
klepsla <kleslava3@gmail.com>
2022-07-29 07:42:19 +0300

    1 задание.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {System.out.println("Hello world!");
+
+    //Задание № 1.
+        String firstName = "Ivan" + " ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov" + " ";
+        String fullName = lastName + firstName + middleName;
+        System.out.print("ФИО сотрудника - " + fullName);
+
+
+
     }
 }
+
