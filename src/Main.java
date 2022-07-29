0a0865258d5e1f97757ea8f68f2842fcf1a87700
klepsla <kleslava3@gmail.com>
2022-07-29 21:39:44 +0300

    Задания выполнены.

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,21 @@ public class Main {
         String middleName = "Ivanovich";
         String lastName = "Ivanov" + " ";
         String fullName = lastName + firstName + middleName;
-        System.out.print("ФИО сотрудника - " + fullName);
-
-
+        System.out.print("ФИО сотрудника - " + fullName + ".");
+        System.out.println();
+    //Задание № 2
+        String s = fullName;
+        String s1 = s.toUpperCase();
+        System.out.print("Данные ФИО сотрудника для заполнения отчёта - " + s1 + ".");
+    //Задание № 3
+        System.out.println();
+        String firstNameS = "Семён" + " ";
+        String middleNameS = "Семёнович";
+        String lastNameS = "Иванов" + " ";
+        String fullNameS = lastNameS + firstNameS + middleNameS;
+        String d = fullNameS;
+        String d1 = d.replace('ё', 'е');
+        System.out.print("Данные ФИО сотрудника - " + d1 + "." );
 
     }
 }
