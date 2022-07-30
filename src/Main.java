2a3f519a590bed74b8a686cd99ae5255e3406711
klepsla <kleslava3@gmail.com>
2022-07-30 19:43:18 +0300

    ДЗ выполнены.

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,20 @@
 public class Main {
-    public static void main(String[] args) {System.out.println("Hello world!");
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
 
-    //Задание № 1.
+        //Задание № 1.
         String firstName = "Ivan" + " ";
         String middleName = "Ivanovich";
         String lastName = "Ivanov" + " ";
         String fullName = lastName + firstName + middleName;
         System.out.print("ФИО сотрудника - " + fullName + ".");
         System.out.println();
-    //Задание № 2
-        String s = fullName;
-        String s1 = s.toUpperCase();
-        System.out.print("Данные ФИО сотрудника для заполнения отчёта - " + s1 + ".");
-    //Задание № 3
+        //Задание № 2
+        System.out.print("Данные ФИО сотрудника для заполнения отчёта - " + fullName.toUpperCase() + ".");
         System.out.println();
-        String firstNameS = "Семён" + " ";
-        String middleNameS = "Семёнович";
-        String lastNameS = "Иванов" + " ";
-        String fullNameS = lastNameS + firstNameS + middleNameS;
-        String d = fullNameS;
-        String d1 = d.replace('ё', 'е');
-        System.out.print("Данные ФИО сотрудника - " + d1 + "." );
-
+        //Задание № 3
+        fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName.replace("ё", "е"));
     }
 }
 
